2103b016dfcaa88555fd3fab215aaa145d5957a8
sarori <sapark@e1z2r6p8.42.us.org>
2019-09-17 16:52:14 -0700

    .

--- d05/ex01/ex01.sql
+++ d05/ex01/ex01.sql
@@ -1,7 +1,6 @@
-CREATE TABLE ft_table( 
-  id bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-  login varchar(11) NOT NULL default 'toto',
-  group ENUM('staff', 'student', 'other'),
-  creation_time datetime,  
-  PRIMARY KEY (id)
+CREATE TABLE ft_table ( 
+  id INT PRIMARY KEY unsigned AUTO_INCREMENT NOT NULL,
+  login varchar(11) default 'toto' NOT NULL,
+  group ENUM('staff', 'student', 'other') NOT NULL,
+  creation_date DATE NOT NULL
 );
